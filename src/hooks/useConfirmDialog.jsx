deac9d2fee9b450e08b6dfb8d8570d27efb5e8e7
Sourav Bishai <souravbishai58558@gmail.com>
2025-12-16 19:20:18 +0530

    show modal header change

--- src/hooks/useConfirmDialog.jsx
+++ src/hooks/useConfirmDialog.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
 import { Modal, Button } from 'rsuite';
+import { Icons } from '../helper/icons';
 
 // Hook
 const useConfirmDialog = () => {
+    // const [state, setState] = useState({
+    //     isOpen: false,
+    //     title: '',
+    //     text: '',
+    //     resolve: null,
+    // });
     const [state, setState] = useState({
-        isOpen: false,
-        title: '',
-        text: '',
+        isOpen: true,
+        title: 'Confirm Action',
+        text: 'Do you want to proceed?',
         resolve: null,
     });
 
@@ -22,11 +29,20 @@ const useConfirmDialog = () => {
     };
 
     const modal = (
-        <Modal open={state.isOpen} onClose={() => handleClose('cancel')} >
-            <Modal.Header>{state.title}</Modal.Header>
+        <Modal open={state.isOpen} onClose={() => handleClose('cancel')} size={'xs'}>
+            {/* <Modal.Header className='border-b'>
+                {state.title}
+            </Modal.Header> */}
+            <div className='flex items-center justify-between pb-1'>
+                <p className='text-xs'>{state.title}</p>
+                <div
+                    onClick={() => handleClose('cancel')}
+                    className='w-[23px] h-[23px] bg-red-400 hover:bg-red-500 grid place-items-center rounded-full cursor-pointer'>
+                    <Icons.CLOSE />
+                </div>
+            </div>
             <Modal.Body>
-                <p></p>
-                <p className='font-bold'>{state.text}</p>
+                <p className='font-semibold'>{state.text}</p>
             </Modal.Body>
             <Modal.Footer>
                 <Button onClick={() => handleClose('cancel')} appearance="subtle" color='red'>
